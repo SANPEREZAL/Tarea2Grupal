28f20b909ec05ed95046d9f65fd459acc7ae08c4
Nicolas Rodriguez <nirodriguezve@unal.edu.co>
2025-04-28 21:10:19 -0500

    Version 2 (agregados carrera y nombre)

---
+++ Tarea2Grupal/src/tarea2grupal/Tarea2Grupal.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tarea2grupal;
+
+/**
+ *
+ * @author Santiago Perez y Nemeque (<--ponga aqui su nombre xd)
+ */
+import java.util.Scanner;
+public class Tarea2Grupal {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Escriba su edad: ");
+        
+        String edad = scanner.nextLine();
+        
+        int a = Integer.parseInt(edad);
+        float b = Float.parseFloat(edad);
+        double c = Double.parseDouble(edad);
+        
+        System.out.println("Su edad convertida a integer es: " + a);
+        System.out.println("Su edad convertida a float es: " + b);
+        System.out.println("Su edad convertida a double es: " + c);
+        
+        System.out.println("Escriba su numero de telefono; ");
+        
+        int telefono = scanner.nextInt();
+        
+        String num = Integer.toString(telefono);
+        
+        System.out.println("Su numero telefonico convertido a str es: " + num);
+        scanner.nextLine();
+        
+        System.out.println("Escriba su carrera: ");
+        String carrera = scanner.nextLine();
+        
+        String carreraFormateada = "Carrera: " + carrera.toUpperCase();
+        System.out.println(carreraFormateada);
+        
+        System.out.println("Escriba su nombre: ");
+        
+        String nombre = scanner.nextLine();
+        
+        System.out.println("Nombre en ASCII:");
+        for (int i = 0; i < nombre.length(); i++) {
+            char letra = nombre.charAt(i);
+            System.out.print((int) letra + " ");
+        }
+    }
+}
